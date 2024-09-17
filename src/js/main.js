a9a6ddb6f13457592860a3131dc6f630031750bb
Gabriel Steven Machicao Quispe <legard2911@gmail.com>
2024-09-17 15:41:52 -0500

    feat: "Add counter de contact"

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
+//Elementos a modificar
 const addButton = document.querySelector('#addContact');
 const contactInput = document.getElementById('contactName');
 const listContact = document.getElementById('contactList');
 
+
+//Variables de contadores
 let index = 0;
+let counter = 0;
+
+const cointerCounter = document.getElementById('cointerCounter');
+cointerCounter.innerText = cointerCounter.innerText = "Cantidad de contactos: " + counter;;
+
 
 const addContact =  function(){
   let contacName = contactInput.value;
@@ -12,15 +20,19 @@ const addContact =  function(){
     return
   }
 
+  //Validar si un nombre no se ha insertado anteriormente
   if(findContact(contacName)){
     alert("Nombre ya ingresado");
     return
   }
   //Crear el LI
 
+  index++;
+  counter++;
   let elementContact = document.createElement('li');
   elementContact.setAttribute("Id", "contacto_"+ index);
 
+  // Etiqueta Span del nombre
   let nameText = document.createElement('span');
   nameText.innerText = contacName;
   elementContact.appendChild(nameText)
@@ -41,26 +53,32 @@ const addContact =  function(){
   })
   elementContact.appendChild(deleteButton);
   listContact.appendChild(elementContact)
+
 }
 
 addButton.addEventListener('click', () => {
-  index++;
   addContact();
   contactInput.value = "";
+  cointerCounter.innerText = "Cantidad de contactos: " + counter;
 });
 
-
-//funciones
+//funcion eliminar
 const deleteContact = function(nameContact){
   const contactos = listContact.getElementsByTagName('li');
   if(contactos.length == 0) return
   for (let i=0; i < contactos.length; i++){
     let contacto = contactos[i].getElementsByTagName('span');
-    if(contacto[0].innerText.trim().toLowerCase() == nameContact.trim().toLowerCase())
-        listContact.removeChild(contactos[i]);
+    if(contacto[0].innerText.trim().toLowerCase() == nameContact.trim().toLowerCase()){
+
+      listContact.removeChild(contactos[i]);
+      counter--;
+    }
   }
+  cointerCounter.innerText = "Cantidad de contactos: " + counter;
 }
 
+
+//funcion editar
 const editContact = function(nameContact){
   const contactos = listContact.getElementsByTagName('li');
   if(contactos.length == 0) return
@@ -78,6 +96,7 @@ const editContact = function(nameContact){
 }
 
 
+//funcion buscar
 const findContact = function(nameContact){
   let contactos = listContact.getElementsByTagName('li');
   if(contactos.length == 0) return false
@@ -88,4 +107,9 @@ const findContact = function(nameContact){
   }
   return false
 }
+
+
+
+
+
 
